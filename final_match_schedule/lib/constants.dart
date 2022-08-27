046334f362b1db966e6fde39ef8a123b0975bfac
Taha Aslam <k190147@nu.edu.pk>
2022-08-27 20:35:10 +0500

    updated constants

---
+++ final_match_schedule/lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kBackgroundColor = Color(0xFFEDEDED);
+const kPrimaryColor = Color(0xFF840C04);
+const kLineColor = Color(0xFFFF9900);
+const kTextColor = Color(0xFF404040);
