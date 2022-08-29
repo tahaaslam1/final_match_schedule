185374536d3da4f49e42beb59270e6c0e81ba48c
hunainarif30 <75844785+hunainarif30@users.noreply.github.com>
2022-08-29 05:37:23 +0500

    match card created (shadows and divider length set random)

--- final_match_schedule/lib/presentation/screens/home_screen/home_screen.dart
+++ final_match_schedule/lib/presentation/screens/home_screen/home_screen.dart
@@ -9,7 +9,62 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Container(),
+        backgroundColor: const Color(0xFFEDEDED),
+        body: Container(
+          height: 52,
+          width: 70,
+          decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: const Color(0xFF000000).withOpacity(0.60),
+                  blurRadius: 4,
+                  spreadRadius: 1,
+                  offset:
+                      const Offset(0.0, 1.0), // shadow direction: bottom right
+                )
+              ],
+              color: Colors.white,
+              border: Border.all(color: Colors.white),
+              borderRadius: const BorderRadius.all(Radius.circular(4))),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'B',
+                style: TextStyle(fontWeight: FontWeight.w700, fontSize: 12),
+              ),
+              const Center(
+                child: Text(
+                  'ENG',
+                  style: TextStyle(fontWeight: FontWeight.w700, fontSize: 10),
+                ),
+              ),
+              const Divider(
+                color: Color(0xFFFFAF00),
+                height: 2.0,
+                indent: 20,
+                thickness: 2,
+                endIndent: 20,
+              ),
+              const Center(
+                child: Text(
+                  'IRN',
+                  style: TextStyle(fontWeight: FontWeight.w700, fontSize: 10),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  const Text(
+                    '13:00',
+                    style: TextStyle(fontWeight: FontWeight.w700, fontSize: 10),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
