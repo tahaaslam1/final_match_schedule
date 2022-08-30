eaf7398d9ae5d868d1bdb86e666340a12ebdb8a5
Taha Aslam <k190147@nu.edu.pk>
2022-08-30 16:59:52 +0500

    basic screen layout..
    setup temp basic layout and added data

--- final_match_schedule/lib/presentation/screens/home_screen/home_screen.dart
+++ final_match_schedule/lib/presentation/screens/home_screen/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -9,7 +8,160 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Container(),
+        body: Row(
+          children: [
+            Expanded(
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        height: 65.h,
+                        width: 71.w,
+                        decoration: const BoxDecoration(
+                          color: Colors.amber,
+                          border: Border(
+                            right: BorderSide(
+                              color: Colors.black,
+                              width: 3.0,
+                            ),
+                            bottom: BorderSide(
+                              color: Colors.black,
+                              width: 3.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          height: 65.h,
+                          decoration: const BoxDecoration(
+                            color: Colors.blue,
+                            border: Border(
+                              bottom: BorderSide(
+                                color: Colors.black,
+                                width: 3.0,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        height: 65.h,
+                        width: 71.w,
+                        decoration: const BoxDecoration(
+                          color: Colors.amber,
+                          border: Border(
+                            right: BorderSide(
+                              color: Colors.black,
+                              width: 3.0,
+                            ),
+                            bottom: BorderSide(
+                              color: Colors.black,
+                              width: 3.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          height: 65.h,
+                          decoration: const BoxDecoration(
+                            color: Colors.blue,
+                            border: Border(
+                              bottom: BorderSide(
+                                color: Colors.black,
+                                width: 3.0,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        height: 65.h,
+                        width: 71.w,
+                        decoration: const BoxDecoration(
+                          color: Colors.amber,
+                          border: Border(
+                            right: BorderSide(
+                              color: Colors.black,
+                              width: 3.0,
+                            ),
+                            bottom: BorderSide(
+                              color: Colors.black,
+                              width: 3.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          height: 65.h,
+                          decoration: const BoxDecoration(
+                            color: Colors.blue,
+                            border: Border(
+                              bottom: BorderSide(
+                                color: Colors.black,
+                                width: 3.0,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        height: 65.h,
+                        width: 71.w,
+                        decoration: const BoxDecoration(
+                          color: Colors.amber,
+                          border: Border(
+                            right: BorderSide(
+                              color: Colors.black,
+                              width: 3.0,
+                            ),
+                            bottom: BorderSide(
+                              color: Colors.black,
+                              width: 3.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          height: 65.h,
+                          decoration: const BoxDecoration(
+                            color: Colors.blue,
+                            border: Border(
+                              bottom: BorderSide(
+                                color: Colors.black,
+                                width: 3.0,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.black,
+              width: 35.0.w,
+            ),
+          ],
+        ),
       ),
     );
   }
